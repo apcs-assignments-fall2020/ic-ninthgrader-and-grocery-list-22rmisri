081614a326632f2ce0bcfc850ef29ee5e362effb
22rmisri <22rmisri@athenian.org>
2020-11-02 20:24:47 +0000

    started work on grocery list

--- src/main/java/GroceryList.java
+++ src/main/java/GroceryList.java
@@ -1,3 +1,22 @@
 public class GroceryList {
+    private String [] arr;
+
+    public GroceryList() {
+        this.arr = new String[10];
+    }
+
+    public void add(String item) {
+        for (int i = 0; i < 10; i++) {
+            if (arr[i] == null) {
+                arr[i] = item;
+                break;
+            }
+
+        }
+    }
+
+    public void remove(String item) {
+        
+    }
     
 }
